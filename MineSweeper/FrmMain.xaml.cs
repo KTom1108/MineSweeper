4f3ffa97ea7b1335621a8259fe5c1ef3a0b7aa66
KTom1108 <ktm911108@gmail.com>
2017-06-09 09:48:20 +0900

    test1
    test1

---
+++ MineSweeper/FrmMain.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MineSweeper
+{
+    /// <summary>
+    /// Interaction logic for FrmMain.xaml
+    /// </summary>
+    public partial class FrmMain : Window
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
