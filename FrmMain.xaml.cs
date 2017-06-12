0f88bdf42a4211c44a57de53af7b909f954eb566
KTom1108 <ktm911108@gmail.com>
2017-06-12 17:38:34 +0900

    Logic update
    Logic update

--- FrmMain.xaml.cs
+++ FrmMain.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
 
 namespace MineSweeper
 {
@@ -21,6 +22,7 @@ namespace MineSweeper
         private OptionEntity optionEntity;
         private FrmOption frmOption;
 
+        private int[] bomb = null;
         public FrmMain()
         {
             InitializeComponent();
@@ -29,7 +31,15 @@ namespace MineSweeper
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            GetOptionValue();
+            GetOptionValue();            
+        }
+
+        private void GameStart() 
+        {
+            CreateArrForMineButton();
+            SetDataForBombButton();
+            SetDataForNumButton();
+            CreateButton();
         }
 
         private void GetOptionValue() 
@@ -42,7 +52,10 @@ namespace MineSweeper
             }
             frmOption.SetOptionEntity(optionEntity);
             frmOption.ShowDialog();
+
             lblLevel.Content = CommonMethod.UseStringBuilder("Level : ", optionEntity.difficultyType, " Class");
+
+            GameStart();
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
@@ -59,5 +72,190 @@ namespace MineSweeper
         {
             this.Close();
         }
+
+        //1.배열생성
+        //2. 난수생성
+
+        private void SetDataForBombButton() 
+        {
+            Random rd = new Random();
+            int rdNum = 0;
+            for (int i = 0; i < optionEntity.numForMine; i++)
+            {
+                rdNum = rd.Next(0, bomb.Length - 1);
+
+                if (bomb[rdNum] == 9)
+                {
+                    i -= 1;
+                }
+                else 
+                {
+                    bomb[rdNum] = 9;
+                }                
+            }
+        }       
+
+        private void SetDataForNumButton() 
+        {
+            for (int i = 0; i < bomb.Length; i++)
+            {
+                if (bomb[i] == 9)
+                {
+                    if (i == 0)
+                    {
+                        if (bomb[i + 1] != 9) { bomb[i + 1] += 1; }
+                        if (bomb[i + optionEntity.hNumForButton] != 9) { bomb[i + optionEntity.hNumForButton] += 1; }
+                        if (bomb[i + optionEntity.hNumForButton + 1] != 9) { bomb[i + optionEntity.hNumForButton + 1] += 1; }
+                    }
+                    else if (i == optionEntity.hNumForButton - 1)
+                    {
+                        if (bomb[i - 1] != 9) { bomb[i - 1] += 1; }
+                        if (bomb[i + optionEntity.hNumForButton - 1] != 9) { bomb[i + optionEntity.hNumForButton - 1] += 1; }
+                        if (bomb[i + optionEntity.hNumForButton] != 9) { bomb[i + optionEntity.hNumForButton] += 1; }
+                    }
+                    else if (i == optionEntity.hNumForButton * (optionEntity.vNumForButton - 1) + 1)
+                    {
+                        if (bomb[i - optionEntity.hNumForButton] != 9) { bomb[i - optionEntity.hNumForButton] += 1; }
+                        if (bomb[i - optionEntity.hNumForButton + 1] != 9) { bomb[i - optionEntity.hNumForButton + 1] += 1; }
+                        if (bomb[i + 1] != 9) { bomb[i + 1] += 1; }
+                    }
+                    else if (i == optionEntity.hNumForButton * optionEntity.vNumForButton - 1)
+                    {
+                        if (bomb[i - optionEntity.hNumForButton - 1] != 9) { bomb[i - optionEntity.hNumForButton - 1] += 1; }
+                        if (bomb[i - optionEntity.hNumForButton] != 9) { bomb[i - optionEntity.hNumForButton] += 1; }
+                        if (bomb[i - 1] != 9) { bomb[i - 1] += 1; }
+                    }
+                    else if (i > 0
+                        && i < optionEntity.hNumForButton - 1)
+                    {
+                        if (bomb[i - 1] != 9) { bomb[i - 1] += 1; }
+                        if (bomb[i + 1] != 9) { bomb[i + 1] += 1; }
+                        if (bomb[i + optionEntity.hNumForButton - 1] != 9) { bomb[i + optionEntity.hNumForButton - 1] += 1; }
+                        if (bomb[i + optionEntity.hNumForButton] != 9) { bomb[i + optionEntity.hNumForButton] += 1; }
+                        if (bomb[i + optionEntity.hNumForButton + 1] != 9) { bomb[i + optionEntity.hNumForButton + 1] += 1; }
+                    }
+                    else if (i > optionEntity.hNumForButton * (optionEntity.vNumForButton - 1) + 1
+                        && i < optionEntity.hNumForButton * optionEntity.vNumForButton - 1)
+                    {
+                        if (bomb[i - 1] != 9) { bomb[i - 1] += 1; }
+                        if (bomb[i + 1] != 9) { bomb[i + 1] += 1; }
+                        if (bomb[i - optionEntity.hNumForButton - 1] != 9) { bomb[i - optionEntity.hNumForButton - 1] += 1; }
+                        if (bomb[i - optionEntity.hNumForButton] != 9) { bomb[i - optionEntity.hNumForButton] += 1; }
+                        if (bomb[i - optionEntity.hNumForButton + 1] != 9) { bomb[i - optionEntity.hNumForButton + 1] += 1; }
+                    }
+                    else if (i % optionEntity.hNumForButton == 0)
+                    {
+                        if (bomb[i - optionEntity.hNumForButton] != 9) { bomb[i - optionEntity.hNumForButton] += 1; }
+                        if (bomb[i - optionEntity.hNumForButton + 1] != 9) { bomb[i - optionEntity.hNumForButton + 1] += 1; }
+                        if (bomb[i + 1] != 9) { bomb[i + 1] += 1; }
+                        if (bomb[i + optionEntity.hNumForButton] != 9) { bomb[i + optionEntity.hNumForButton] += 1; }
+                        if (bomb[i + optionEntity.hNumForButton + 1] != 9) { bomb[i + optionEntity.hNumForButton + 1] += 1; }
+                    }
+                    else if (i % optionEntity.hNumForButton == optionEntity.hNumForButton - 1)
+                    {
+                        if (bomb[i - optionEntity.hNumForButton - 1] != 9) { bomb[i - optionEntity.hNumForButton - 1] += 1; }
+                        if (bomb[i - optionEntity.hNumForButton] != 9) { bomb[i - optionEntity.hNumForButton] += 1; }
+                        if (bomb[i - 1] != 9) { bomb[i - 1] += 1; }
+                        if (bomb[i + optionEntity.hNumForButton - 1] != 9) { bomb[i + optionEntity.hNumForButton - 1] += 1; }
+                        if (bomb[i + optionEntity.hNumForButton] != 9) { bomb[i + optionEntity.hNumForButton] += 1; }
+                    }
+                    else
+                    {
+                        if (bomb[i - optionEntity.hNumForButton - 1] != 9) { bomb[i - optionEntity.hNumForButton - 1] += 1; }
+                        if (bomb[i - optionEntity.hNumForButton] != 9) { bomb[i - optionEntity.hNumForButton] += 1; }
+                        if (bomb[i - optionEntity.hNumForButton + 1] != 9) { bomb[i - optionEntity.hNumForButton + 1] += 1; }
+                        if (bomb[i - 1] != 9) { bomb[i - 1] += 1; }
+                        if (bomb[i + 1] != 9) { bomb[i + 1] += 1; }
+                        if (bomb[i + optionEntity.hNumForButton - 1] != 9) { bomb[i + optionEntity.hNumForButton - 1] += 1; }
+                        if (bomb[i + optionEntity.hNumForButton] != 9) { bomb[i + optionEntity.hNumForButton] += 1; }
+                        if (bomb[i + optionEntity.hNumForButton + 1] != 9) { bomb[i + optionEntity.hNumForButton + 1] += 1; }
+                    }
+                }
+            }
+        }
+
+        private void CreateArrForMineButton() 
+        {
+            bomb = new int[optionEntity.hNumForButton * optionEntity.vNumForButton];
+        }
+
+        private void CreateButton() 
+        {
+            for (int i = 0; i < optionEntity.hNumForButton; i++)
+            {
+                mainGrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1.0, GridUnitType.Star) });
+            }
+
+            for (int i = 0; i < optionEntity.vNumForButton; i++)
+            {
+                mainGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1.0, GridUnitType.Star) });
+            }
+
+            var imgBrush = new ImageBrush();
+            imgBrush.ImageSource = new BitmapImage(new Uri(@"../../Image/Bomb.ico", UriKind.Relative));
+
+            for (int i = 0; i < bomb.Length; i++)
+            {
+                var mineButton = new ToggleButton();
+                
+                mineButton.Name = CommonMethod.UseStringBuilder("mine", i.ToString());
+                mineButton.Background = Brushes.Gray;
+                mineButton.IsChecked = true;
+                if (bomb[i] == 9)
+                {
+                    mineButton.Background = imgBrush;
+                }
+                else 
+                {
+                    mineButton.Content = bomb[i].ToString();
+                    mineButton.FontSize = 14;
+                    mineButton.FontWeight = FontWeights.Bold;
+
+                    if (bomb[i] == 1 )
+                    {
+                        mineButton.Foreground = Brushes.Blue;
+                    }
+                    else if (bomb[i] == 2)
+                    {
+                        mineButton.Foreground = Brushes.Green;
+                    }
+                    else if (bomb[i] == 3)
+                    {
+                        mineButton.Foreground = Brushes.Orange;
+                    }
+                    else if (bomb[i] == 4)
+                    {
+                        mineButton.Foreground = Brushes.Brown;
+                    }
+                    else if (bomb[i] == 5)
+                    {
+                        mineButton.Foreground = Brushes.Pink;
+                    }
+
+                    else if (bomb[i] == 6)
+                    {
+                        mineButton.Foreground = Brushes.Black;
+                    }
+
+                    else if (bomb[i] == 7)
+                    {
+                        mineButton.Foreground = Brushes.Gray;
+                    }
+
+                    else if (bomb[i] == 8)
+                    {
+                        mineButton.Foreground = Brushes.Gold;
+                    }
+                }
+
+                Grid.SetRow(mineButton, i / optionEntity.vNumForButton);
+                Grid.SetColumn(mineButton, i % optionEntity.vNumForButton);
+
+                mainGrid.Children.Add(mineButton);
+            }
+
+
+        }
+
     }
 }
